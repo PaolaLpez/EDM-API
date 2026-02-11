2adfe68b912355bb472f5138cdcb2bca0b2531b7
usuario en Github <correo>
2026-02-10 19:58:20 -0600

    Uso de validaciones y CRUD funcional

--- src/modules/task/task.controller.ts
+++ src/modules/task/task.controller.ts
@@ -9,38 +9,34 @@ import {
 } from '@nestjs/common';
 
 import { TaskService } from './task.service';
-
-interface Task {
-  title: string;
-  description: string;
-}
+import type { Task } from './task.service';
 
 @Controller('api/tasks')
 export class TaskController {
   constructor(private readonly taskSvc: TaskService) {}
 
   @Get()
-  public getTasks(): string {
-    return this.taskSvc.getTasks();
+  public getTasks(): Task[] {
+    return this.taskSvc.getTask();
   }
 
   @Get(':id')
   public getTaskById(@Param('id') id: string): string {
-    return this.taskSvc.getTaskById(parseInt(id));
+    return this.taskSvc.getTaskById(Number(id));
   }
 
   @Post()
-  public insertTask(@Body() task: any) {
+  public insertTask(@Body() task: Task): Task {
     return this.taskSvc.insert(task);
   }
 
-  @Put('/:id')
-  public updateTask(@Param("id") id:string, @Body() task: any) {
-    return this.taskSvc.update(parseInt(id));
+  @Put(':id')
+  public updateTask(@Param('id') id: string, @Body() task: Task): Task {
+    return this.taskSvc.update(Number(id), task);
   }
 
   @Delete(':id')
-  deleteTask(@Param('id') id: string): string {
-    return this.taskSvc.delete(parseInt(id));
+  public deleteTask(@Param('id') id: string): string {
+    return this.taskSvc.delete(Number(id));
   }
 }
--- src/modules/task/task.service.ts
+++ src/modules/task/task.service.ts
@@ -1,24 +1,42 @@
 import { Injectable } from '@nestjs/common';
 
+export interface Task {
+  id: number;
+  title: string;
+}
+
 @Injectable()
 export class TaskService {
-  public getTasks(): string {
-    return 'Lista de tareas';
+  private tasks: Task[] = [];
+
+  public getTask(): Task[] {
+    return this.tasks;
   }
 
   public getTaskById(id: number): string {
     return `Tarea con el id ${id}`;
   }
 
-  public insert(task: any): any {
+  public insert(task: Task): Task {
+    this.tasks.push(task {
+      ...task,
+      id: this.tasks.length + 1
+    }
     return task;
   }
 
-  public update(id: number, task: any): any {
-    return { id, ...task };
+  public update(id: number, task: Task): Task {
+    const index = this.tasks.map(t => {
+    if (t.id == id) {
+      if (task.name) t.name = task.name;
+      }
+    }
+    return t;
+
   }
 
   public delete(id: number): string {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
     return `Tarea con el id ${id} eliminada`;
   }
 }
